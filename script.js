2f93cab3b2cf5f6ac34121bba6d5809b91777a01
Gideon <abaaneg@gmail.com>
2023-01-20 21:47:49 +0000

    almost done

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ const todos = document.getElementById("todos")
 form.addEventListener("submit", (e) => {
      e.preventDefault()
 
+    addToDo()
+
+})
+
+
+function addToDo () {
     const todoText = input.value
-   console.log(todoText) 
     if(todoText) {
         const todoEl = document.createElement("li")
         todoEl.innerText = todoText
@@ -25,6 +30,24 @@ form.addEventListener("submit", (e) => {
         todos.appendChild(todoEl)
 
         input.value = ""
+
+        updateLS()
     }
 
-})
+}
+
+
+function updateLS() {
+    const todosEl = document.querySelectorAll('li')
+    
+    const todos = []
+
+    todosEl.forEach((todoEl) => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.toggle.contains("completed")
+        })
+    })
+
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
