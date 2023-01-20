7bea48fd7698890871398e6a009c4889389b97e5
Gideon <abaaneg@gmail.com>
2023-01-20 20:56:13 +0000

    see added to list

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const form = document.getElementById('form')
-const input = document.getElementById('input')
+const form = document.getElementById("form")
+const input = document.getElementById("input")
+const todos = document.getElementById("todos")
 
-form.addEventListener('submit', (e) =>{
-    e.preventDefault()
 
-    const todo = input.ariaValueMax
-    if(todo) {
+form.addEventListener("submit", (e) => {
+     e.preventDefault()
+
+    const todoText = input.value
+   console.log(todoText) 
+    if(todoText) {
+        const todoEl = document.createElement("li")
+        todoEl.innerText = todoText
+        todos.appendChild(todoEl)
          
     }
 
